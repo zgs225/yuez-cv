554aeb87fff2287e476d4b2cf72489d0af057517
zgs225 <zgs225@gmail.com>
2016-12-04 16:19:15 +0800

    Add canny transform

--- scale_half.c
+++ scale_half.c
@@ -3,7 +3,7 @@
 
 #define WIN_NAME "Scale Down Image"
 
-IplImage *scale_down(IplImage *in, int filter)
+IplImage* scale_down(IplImage *in, int filter)
 {
     assert(in->width % 2 == 0 && in->height % 2 == 0);
 
@@ -16,15 +16,27 @@ IplImage *scale_down(IplImage *in, int filter)
     return out;
 }
 
+IplImage* canny_edge_recognize(IplImage *in, 
+                               double lowThresh, 
+                               double highThresh, 
+                               int aperture)
+{
+    IplImage *out = cvCreateImage(cvGetSize(in), IPL_DEPTH_8U, 1);
+    cvCanny(in, out, lowThresh, highThresh, aperture);
+    return out;
+}
+
 int main(int argc, const char *argv[])
 {
     IplImage *in = cvLoadImage(argv[1], CV_LOAD_IMAGE_COLOR);
     IplImage *out = scale_down(in, IPL_GAUSSIAN_5x5);
+    IplImage *out2 = canny_edge_recognize(out, 10, 100, 3);
     cvNamedWindow(WIN_NAME, CV_WINDOW_AUTOSIZE);
-    cvShowImage(WIN_NAME, out);
+    cvShowImage(WIN_NAME, out2);
     cvReleaseImage(&in);
     cvWaitKey(0);
     cvReleaseImage(&out);
+    cvReleaseImage(&out2);
     cvDestroyWindow(WIN_NAME);
     return 0;
 }
